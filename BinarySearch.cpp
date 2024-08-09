f435f27195de3c297ac2c4018b2991da8e28f494
Vatsalv19 <141351992+Vatsalv19@users.noreply.github.com>
2024-08-09 17:41:05 +0530

    BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,55 +1,30 @@
 #include<stdio.h>
 
-//int binarySearch(int arr[],int size,int key){
-//	int start = 0;
-//	int end = size-1;
-//	int mid = (start+end/2);
-//	
-//	while(start<=end){
-//		if(arr[mid]==key){
-//			return mid;
-//		}
-//		//GO TO RIGHT PART
-//		if(key>arr[mid]){
-//			start = mid+1;
-//		}
-//		else{
-//			end = mid-1;
-//		}
-//		mid = (start+end)/2;
-//	}
-//	return -1;
-//}
-//
-//int main(){
-//	int even[5] = {2,4,6,8,10};
-//	int index = binarySearch(even,5,4);
-//	printf("index is 4 is %d ",index);
-//	return 0;
-//}
-
-//Practice by Me
-
-binarySearch(int arr[],int size,int key){
+int binarySearch(int arr[],int size,int key){
 	int start = 0;
 	int end = size-1;
-	int mid = (start+end)/2;
-		while(start<=end){
-			if(arr[mid]==key){
-				return mid;
-			}
-			//right
-			if(key>arr[mid]){
-				start = mid+1;
-			}
-			else{
-				end = mid-1;
-			}
+	int mid = (start+end/2);
+	
+	while(start<=end){
+		if(arr[mid]==key){
+			return mid;
 		}
-		return -1;
+		//GO TO RIGHT PART
+		if(key>arr[mid]){
+			start = mid+1;
+		}
+		else{
+			end = mid-1;
+		}
+		mid = (start+end)/2;
+	}
+	return -1;
 }
 
 int main(){
-	int arr[5] = {1,2,3,4,5};
-	binarySearch(arr,5,4);
+	int even[5] = {2,4,6,8,10};
+	int index = binarySearch(even,5,4);
+	printf("index is 4 is %d ",index);
+	return 0;
 }
+
